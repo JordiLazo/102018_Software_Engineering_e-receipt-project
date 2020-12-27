61bbb4043468ff498d2aa62f03d8dfcc1965392f
Jordi Lazo <jordilazo94@gmail.com>
2020-12-27 13:35:37 +0100

    PSVM

--- src/data/HealthCardID.java
+++ src/data/HealthCardID.java
@@ -6,27 +6,35 @@ final public class HealthCardID {
     public HealthCardID(String code) {
         this.personalID = code;
     }
-    public String getPersonalID(){
+
+    public String getPersonalID() {
         return personalID;
     }
+
     @Override
-    public boolean equals(Object o ){
-        if (this == o){
+    public boolean equals(Object o) {
+        if (this == o) {
             return true;
         }
-        if(o == null || getClass() != o.getClass()){
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
         HealthCardID hcardID = (HealthCardID) o;
         return personalID.equals(hcardID.personalID);
     }
+
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return personalID.hashCode();
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "HealthCardID{" + "personal code='" + personalID + '\'' + '}';
+    }
+
+    public static void main(String[] args) {
+        HealthCardID test = new HealthCardID("acacasc");
+        System.out.println(test);
     }
 }
