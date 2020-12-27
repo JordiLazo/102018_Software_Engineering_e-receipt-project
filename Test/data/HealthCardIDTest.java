772f214c0f6c5e89609275c093e7fd514c7f8e7c
sys-walker <pere.rollon@gmail.com>
2020-12-27 13:23:49 +0100

    Added Test

---
+++ Test/data/HealthCardIDTest.java
@@ -0,0 +1,18 @@
+package data;fiy
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HealthCardIDTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPersonalID() {
+    }
+}
