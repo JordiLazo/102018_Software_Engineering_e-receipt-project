f188e25527c441a9717f66966c29044f8820dff7
Jordi Lazo <jordilazo94@gmail.com>
2020-12-27 12:39:35 +0100

    Java

--- src/data/HealthCardID.java
+++ src/data/HealthCardID.java
@@ -1,4 +1,30 @@
 package data;
 
-public class HealthCardID {
+final public class HealthCardID {
+    private final String personalID;
+
+    public HealthCardID(String code) {
+        this.personalID = code;
+    }
+    public String getPersonalID(){
+        return personalID;
+    }
+
+    public boolean equals(Object o ){
+        if (this == o){
+            return true;
+        }
+        if(o == null || getClass() != o.getClass()){
+            return false;
+        }
+        HealthCardID hcardID = (HealthCardID) o;
+        return personalID.equals(hcardID.personalID);
+    }
+    public int hashCode(){
+        return personalID.hashCode();
+    }
+
+    public String toString(){
+        return "HealthCardID{" + "personal code='" + personalID + '\'' + '}';
+    }
 }
