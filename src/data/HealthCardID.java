f1ec6379e659b1095b2361f1c347b311cee5fe4e
Jordi Lazo <jordilazo94@gmail.com>
2020-12-28 16:04:50 +0100

    HealthCardID checkPersonalID

--- src/data/HealthCardID.java
+++ src/data/HealthCardID.java
@@ -1,26 +1,46 @@
 package data;
 
+import exceptions.HealthCardException;
 import exceptions.NullNotDefinedException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Locale;
 
 final public class HealthCardID{
     private final String personalID;
 
-    public HealthCardID(String code) throws NullNotDefinedException {
+    public HealthCardID(String code) throws NullNotDefinedException, HealthCardException {
         this.personalID = this.checkPersonalID(code);
     }
 
-    private String checkPersonalID(String code) throws NullNotDefinedException {
+    private String checkPersonalID(String code) throws NullNotDefinedException, HealthCardException {
+        code = code.toUpperCase();
+        String newcode1 = code.substring(8,10);
+        String newcode2 = code.substring(10,28);
+        System.out.println(code);
+        System.out.println(newcode1);
+        System.out.println(newcode2);
+        if(code.length() != 28){
+            throw new HealthCardException("Invalid HealthCardID. Enter 28 digits.");
+        }
+        if (!code.startsWith("BBBBBBBB")){
+            throw new HealthCardException("Invalid HealthCardID. Wrong 8 digits");
+        }
+        if (!newcode1.matches("[A-Z]*")){
+            throw new HealthCardException("Invalid 2 letters");
+        }
+        if (!newcode2.matches("[0-9]+")){
+            throw new HealthCardException("Invalid 18 digits");
+        }
         if (code != null){
             return code;
         }
         throw new NullNotDefinedException("missatge");
-
     }
 
     public String getPersonalID() {
@@ -50,12 +70,12 @@ final public class HealthCardID{
             return "HealthCardID {" + "personal code='" + personalID + '\'' + '}';
         }
         return "";
-
     }
 
-    public static void main(String[] args) throws NullNotDefinedException, ParseException {
+    public static void main(String[] args) throws NullNotDefinedException, ParseException, HealthCardException {
         Date n= new SimpleDateFormat( "yyyy-MM-dd" ).parse( "2010-05-20" );
         System.out.println(n);
-
+        HealthCardID z = new HealthCardID("BBBBBBBBPR148597807024001012");
+        System.out.println(z);
     }
 }
