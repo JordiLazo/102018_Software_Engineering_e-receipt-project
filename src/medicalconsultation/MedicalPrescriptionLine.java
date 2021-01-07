62d2000029898d3ab5cec739d9f04e1cfa56e466
sys-walker <pere.rollon@gmail.com>
2021-01-07 15:20:35 +0100

    Implementation of MedicalPrescriptionLine

--- src/medicalconsultation/MedicalPrescriptionLine.java
+++ src/medicalconsultation/MedicalPrescriptionLine.java
@@ -1,10 +1,43 @@
 package medicalconsultation;
 
-import data.ProductID;
+import data.*;
+import exceptions.*;
+import java.util.HashMap;
 
 public class MedicalPrescriptionLine {
-    TakingGuideline s;
-    ProductID p;
+    public final HashMap<ProductID, String[]> map;
+
+    public MedicalPrescriptionLine() {
+        this.map = new HashMap<>();
+    }
+
+    public void addLine(ProductID prodID,String[] instruc)throws IncorrectTakingGuidelinesException {
+        if(!map.containsKey(prodID)){
+            if (prodID != null && instruc.length!=0) {
+                map.put(prodID,instruc);
+            }else {
+                throw new IncorrectTakingGuidelinesException("Cannot add: Incorrect Format");
+            }
+
+        }
+    }
+    public void modifyLine(ProductID prodID,String[] instruc) throws ProductNotInPrescription, IncorrectTakingGuidelinesException{
+        if(map.containsKey(prodID)){
+            if (prodID != null && instruc.length!=0) {
+                map.put(prodID,instruc);
+            }else {
+                throw new IncorrectTakingGuidelinesException("Cannot modify: Incorrect Format");
+            }
+        }
+        throw new ProductNotInPrescription("Cannot modify: Not found product ");
+    }
+    public void removeLine(ProductID prodID,String[] instruc) throws ProductNotInPrescription{
+        if(map.containsKey(prodID)){
+            map.remove(prodID);
+        }else{
+            throw new ProductNotInPrescription("Cannot remove non existent product ");
+        }
+    }
 
 }
 /*
